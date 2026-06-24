838e57e2b34b60046cdfaf2c18aaae7741e491f9
Nikesh Pandey <nikeshhzb7461@gmail.com>
2026-06-24 17:15:46 +0530

    Update

--- reviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Review
-
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ("email", "rating", "comment", "created_at")
-    search_fields = ("email", "comment")
-    list_filter = ("rating", "created_at")
